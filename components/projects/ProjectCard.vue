2b21d39e0687ecdb6047a588187d6d39103351f6
Jacob Ekanem <prometheus6001@gmail.com>
2024-01-30 04:25:44 -0400

    Project view page added with validaion and basic view layout

---
+++ components/projects/ProjectCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Project } from '~/types/project'
+import ProjectIcon from './ProjectIcon.vue'
+
+defineProps<{ project: Project }>()
+</script>
+
+<template>
+  <div
+    class="bg-white dark:bg-gray-900 ring-1 ring-gray-200 dark:ring-gray-800 rounded-md shadow px-6 py-5 hover:bg-white/50 dark:hover:bg-gray-900/50 transition-all ease-out duration-200"
+  >
+    <div class="flex justify-between">
+      <h4 class="font-medium mb-3">{{ project.project_name }}</h4>
+      <ProjectIcon :project="project" />
+    </div>
+    <p class="text-xs mt-1 truncate">{{ project.project_description }}</p>
+  </div>
+</template>
---
+++ components/projects/view/ProjectHeader.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import type { Project } from '~/types/project'
+import ProjectIcon from '~/components/projects/ProjectIcon.vue'
+
+defineProps<{
+  project: Project
+}>()
+const router = useRouter()
+
+const back = () => {
+  router.back()
+}
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center">
+      <UButton
+        @click="back"
+        icon="i-heroicons-arrow-small-left"
+        size="xs"
+        class="mr-3"
+        :ui="{ rounded: 'rounded-full' }"
+        >back</UButton
+      >
+      <h3 class="text-3xl font-extrabold mr-3">{{ project.project_name }}</h3>
+      <ProjectIcon class="mt-3" :project="project" />
+    </div>
+    <div class="w-1/3 mt-3">
+      <small>{{ project.project_description }}</small>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
